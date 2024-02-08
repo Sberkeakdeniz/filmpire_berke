6943e4f5c05acc9ec16a1d00886a669f39a244b3
Seralp Berke Akdeniz <berkeakdnzz@gmail.com>
2024-02-08 14:26:29 +0300

    authentication(user login and logout is finalized)

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Typography, Button, Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
+import { userSelector } from '../../features/auth';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+const Profile = () => {
+  const { user } = useSelector(userSelector);
+
+  const favouriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>My Profile</Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favouriteMovies.length ? <Typography variant="h5">You have no favourite movies, please add one or more...</Typography> : <Box>Favourite Movies!</Box>}
+    </Box>
+  );
+};
 
 export default Profile;
