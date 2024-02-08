f1fcc888c718ac720f2590233d371720cf5f73c0
Seralp Berke Akdeniz <berkeakdnzz@gmail.com>
2024-02-08 10:19:15 +0300

    switching genres directly throughout from api is added

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { tmdbApi } from '../services/TMDB';
+import genreOrCategoryReducer from '../features/currentGenreOrCategory';
 
 // a file that exports a function that creates a store
 export default configureStore({
-    reducer: {
-        [tmdbApi.reducerPath]: tmdbApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(tmdbApi.middleware),
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+    currentGenreOrCategory: genreOrCategoryReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
