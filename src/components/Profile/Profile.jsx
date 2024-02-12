0e5c87547c8ff02e86f9064e3370b68d636957fa
Seralp Berke Akdeniz <berkeakdnzz@gmail.com>
2024-02-12 11:03:58 +0300

    favorite and watchlist functionalities are added to their buttons and also the code to display them in the profile page is added.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable import/no-cycle */
 import React, { useEffect } from 'react';
 import { Typography, Button, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { ExitToApp } from '@mui/icons-material';
+
+import { useGetListQuery } from '../../services/TMDB';
 import { userSelector } from '../../features/auth';
+import { RatedCards } from '..';
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
-  const favouriteMovies = [];
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({ listName: 'favorite/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+  const { data: watchListMovies, refetch: refetchWatchlisted } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+
+  // this refetch the data without reloading the page (for displaying the current favorite movies at the profile page)
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlisted();
+  }, []);
 
   const logout = () => {
     localStorage.clear();
@@ -23,7 +34,14 @@ const Profile = () => {
           Logout &nbsp; <ExitToApp />
         </Button>
       </Box>
-      {!favouriteMovies.length ? <Typography variant="h5">You have no favourite movies, please add one or more...</Typography> : <Box>Favourite Movies!</Box>}
+      {!favoriteMovies?.results?.length && !watchListMovies?.results?.length
+        ? <Typography variant="h5">You have no favourite movies, please add one or more...</Typography>
+        : (
+          <Box>
+            <RatedCards title="Favorite Movies" data={favoriteMovies} />
+            <RatedCards title="Watchlist" data={watchListMovies} />
+          </Box>
+        )}
     </Box>
   );
 };
